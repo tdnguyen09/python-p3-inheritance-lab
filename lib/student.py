2b1aba6bf9432b1936e2ca6757ed8e0f7b21e205
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-05-03 22:38:09 +1000

    complete lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,9 @@
 from user import User
 
 class Student(User):
+    def __init__(self, last_name, first_name):
+        super().__init__(last_name, first_name)
+        self.knowledge = [] 
     
-    def learn(self):
-        pass
+    def learn(self, string):
+        self.knowledge.append(string)
